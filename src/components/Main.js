8496f695a71d25779a0211732b0709598cb6ead2
Dad 2 JRN <dad2jrn@gmail.com>
2019-09-02 08:39:23 -0400

    capital one logo

--- src/components/Main.js
+++ src/components/Main.js
@@ -23,7 +23,7 @@ class Main extends React.Component {
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Career</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
+          <span className="image main"><img src={Capital_One_logo.svg} alt="" /></span>
           <p>This is my work Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
           <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
           <span className="image main"><img src={pic02} alt="" /></span>
